98f2db28297e8ed5e52de6056a47c87c61eb6fe8
Lê Khánh Trình <vulekhanhtrinh2003@gmail.com>
2025-03-06 21:35:43 +0700

    initialize Next.js blog project with Tailwind CSS and Bootstrap

---
+++ blog/app/[id]/page.jsx
@@ -0,0 +1,43 @@
+// pages/[id].js
+'use client';
+import React, { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const BlogDetails = () => {
+  const [blogDetail, setBlogDetail] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const blogs = JSON.parse(localStorage.getItem("myData"));
+    const selectedBlog = blogs.find((blog) => blog.id === parseInt(id));
+    setBlogDetail(selectedBlog);
+  }, [id]);
+
+  if (!blogDetail) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="container bg-light" style={{ marginTop: "5rem" }}>
+      <div className="card mt-5">
+        <img
+          src={blogDetail.imageUrl}
+          style={{
+            maxWidth: "100%",
+            maxHeight: "300px",
+          }}
+          className="card-img-top"
+          alt="Blog"
+        />
+        <div className="card-body">
+          <h1 className="card-title">{blogDetail.title}</h1>
+          <p className="card-text">{blogDetail.description}</p>
+          <p className="card-text">Author: {blogDetail.author}</p>
+          <p className="card-text">Date: {blogDetail.date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDetails;
---
+++ blog/app/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark bg-opacity-75 fixed-top text-light">
+        <div className="container">
+          <Link className="navbar-brand text-light font-bold" href="/">
+            Blogger
+          </Link>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link href="/" className="nav-item nav-link text-light">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/createblog" className="nav-item nav-link text-light">
+                Create new Blog
+              </Link>
+            </li>
+          </ul>
+          {/* <Link href="/about">About</Link>
+          <Link href="/blog">Blog</Link> */}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
